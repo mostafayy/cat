16d1d4cee9f625f53b257225659940c0f6ad1cab
Your Nameclear <you@example.com>
2022-07-16 11:11:46 +0200

    craete cat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Home from './components/Home'
+import './App.css'
+import Clients from './components/Clients'
+import CreateAndSell from './components/CreateAndAsell'
+import Choose from './components/Choose'
+import MarketPlace from './components/MarketPlace'
+import Subscribe from './components/Subscribr'
+import ScrollToTop from './components/ScrollToTop'
+import Footer from './components/Footer'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ScrollToTop/>
+      <Home/>
+      <Clients/>
+      <CreateAndSell/>
+      <Choose/>
+      <MarketPlace/>
+      <Subscribe/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
